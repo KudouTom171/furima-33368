909ffb401a38a75fc06ce287431f7b7285fded06
KudouTom171 <76691187+KudouTom171@users.noreply.github.com>
2021-03-28 17:17:20 +0900

    バリデーションの修正、テストコードの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,21 +7,27 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :birthday, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :last_name
+    validates :first_name
+    validates :last_name_kana
+    validates :first_name_kana
+    validates :birthday
+  end
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: '英字と数字の両方を含めて設定してください' 
+  validates_format_of :password, with: PASSWORD_REGEX, message: '半角英字と半角数字の両方を含めて設定してください' 
 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
     validates :first_name
     validates :last_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶーー]+\z/, message: '全角カタカナを使用してください' } do
     validates :first_name_kana
     validates :last_name_kana
   end
+
 
 end
